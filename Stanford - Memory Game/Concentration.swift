39cb1db797d2dae95107aace7e55af8145cd2e97
torresrodrigo <dev.rodrigotorres@gmail.com>
2021-02-01 03:34:55 -0300

    2da Parte
    Se agrega patrón de diseño MVC, como así tambien la logica para que puedan coincidir entre las cartas.

---
+++ Stanford - Memory Game/Concentration.swift
@@ -0,0 +1,43 @@
+//
+//  Concentration.swift
+//  Stanford - Memory Game
+//
+//  Created by Rodrigo Torres on 31/01/2021.
+//
+
+import Foundation
+
+class Concentration {
+    
+    var cards = [Card]()
+    
+    var indexOfOneAndOnlyFaceUpCard: Int?
+    
+    func chooseCard(at index: Int) {
+        if !cards[index].isMatched {
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = nil
+            } else {
+                for flipDownIndex in cards.indices {
+                    cards[flipDownIndex].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
+        }
+    }
+    
+    init(numbersOfPairsOfCards: Int) {
+        for _ in 1...numbersOfPairsOfCards {
+            let card = Card()
+            cards += [card, card]
+        }
+    }
+    
+    //Todo: Shuffle the Cards
+}
--- Stanford - Memory Game/ViewController.swift
+++ Stanford - Memory Game/ViewController.swift
@@ -9,38 +9,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    lazy var game = Concentration(numbersOfPairsOfCards: (cardButtons.count + 1) / 2)
 
-    var flipCount = 0 {
-        didSet {
-            flipCountLabel.text = "Flips : \(flipCount)"
-        }
-    }
+    var flipCount = 0 { didSet { flipCountLabel.text = "Flips : \(flipCount)" } }
     
     @IBOutlet weak var flipCountLabel : UILabel!
     
     @IBOutlet var cardButtons: [UIButton]!
     
-    var emojiChoices = ["👺","👻","👺","👻"]
-    
-    
+
     @IBAction func touchCard(_ sender: UIButton) {
         flipCount += 1
         if let cardNumber = cardButtons.firstIndex(of: sender) {
-            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+            game.chooseCard(at: cardNumber)
+            updateViewFromModel()
         } else {
             print("Chosen card was no in CardButtons")
         }
     }
     
-    func flipCard(withEmoji emoji: String, on button: UIButton) {
-        if button.currentTitle == emoji {
-            button.setTitle("", for: UIControl.State.normal)
-            button.backgroundColor = #colorLiteral(red: 0.9508606791, green: 0.1432128251, blue: 0, alpha: 1)
-        } else {
-            button.setTitle(emoji, for: UIControl.State.normal)
-            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    func updateViewFromModel() {
+        for index in cardButtons.indices {
+            let button = cardButtons[index]
+            let card = game.cards[index]
+            if card.isFaceUp {
+                button.setTitle(emoji(for: card), for: UIControl.State.normal)
+                button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+                print()
+            } else {
+                button.setTitle("", for: UIControl.State.normal)
+                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0) : #colorLiteral(red: 0.9508606791, green: 0.1432128251, blue: 0, alpha: 1)
+            }
         }
     }
+    
 
+    var emojiChoices = ["👺","👻","🕶","🎩","💍","💼"]
+    
+    var emoji = [Int:String]()
+    
+    func emoji(for card: Card) -> String {
+        if emoji[card.identifier] == nil, emojiChoices.count > 0 {
+            let ramdomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
+            emoji[card.identifier] = emojiChoices.remove(at: ramdomIndex)
+        }
+        return emoji[card.identifier] ?? "?"
+    }
 
 }
+
